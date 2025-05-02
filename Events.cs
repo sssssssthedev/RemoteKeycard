69e6634f9c4073917813ca2e806e66b0e8eab025
sssssssthedev <sssssssthedev@gmail.com>
2025-05-02 12:42:08 +0300

    fix minor spelling mistake in debuggin output

--- Events.cs
+++ Events.cs
@@ -69,7 +69,7 @@ public class Events : CustomEventsHandler
           { 
                if (RemoteKeycard.Instance.Config.Debug)
                {
-                    Logger.Debug($"OnPlayerInteractingDoor(): Keycard in player inventory does not have permsision to open door (KeycardName: {keycard.Name}, DoorName: {doorBase.DoorName}, KeycardHasPermsForDoor: {Utils.KeycardHasPermissionForDoor(keycard, player, doorBase)})");
+                    Logger.Debug($"OnPlayerInteractingDoor(): Keycard in player inventory does not have permission to open door (KeycardName: {keycard.Name}, DoorName: {doorBase.DoorName}, KeycardHasPermsForDoor: {Utils.KeycardHasPermissionForDoor(keycard, player, doorBase)})");
                }
                return;
           }
@@ -182,7 +182,7 @@ public class Events : CustomEventsHandler
           { 
                if (RemoteKeycard.Instance.Config.Debug)
                {
-                    Logger.Debug($"OnPlayerInteractingGenerator(): Keycard in player inventory does not have permsision to open generator (KeycardName: {keycard.Name}, GeneratorName: {generatorBase.name}, KeycardHasPermsForDoor: {Utils.KeycardHasPermissionForGenerator(keycard, player, generatorBase)})");
+                    Logger.Debug($"OnPlayerInteractingGenerator(): Keycard in player inventory does not have permission to open generator (KeycardName: {keycard.Name}, GeneratorName: {generatorBase.name}, KeycardHasPermsForDoor: {Utils.KeycardHasPermissionForGenerator(keycard, player, generatorBase)})");
                }
                return;
           }
